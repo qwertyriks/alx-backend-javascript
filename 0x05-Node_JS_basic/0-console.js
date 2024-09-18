7be55c9a04b29c84bedc0273af9390570c5cd86b
qwertyriks <romeokumatse@gmail.com>
2024-09-18 21:00:33 +0000

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display the message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Isaaac Kumatse <https://github.com/qwertyriks>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
